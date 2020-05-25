931c9d9d1a1a21eee8b7ab66d34cc3bcea06abd8
gmargaritis <gmargaritis.dev@gmail.com>
2020-05-25 18:38:46 +0300

    Deploying to netlify

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import App from "./App";
-import Quiz from "./quiz";
+import Quiz from './Components/Quiz';
 
 const Routes = () => {
   return (
